6624a2c1c57dc9343f516e0df99cad62a19afe40
Robert C. Cain <arcane@arcanetc.com>
2018-03-26 17:19:55 -0500

    Add files via upload

---
+++ PSAzure_Module/PSAzure-Deploy/Install-PSAzureModule.ps1
@@ -0,0 +1,30 @@
+﻿$moduleName = 'PSAzure'
+$moduleFolder = 'PSAzure-Module'
+
+$oneDrive = $env:OneDrive
+
+$rootLocation = "$onedrive\Pluralsight\$moduleFolder"
+$filesLocation = "$rootLocation\$moduleName"
+
+Push-Location
+Set-Location $rootLocation
+
+. .\DeployModule.ps1
+
+$files = Get-ChildItem $filesLocation
+
+$moduleFiles = @()
+foreach ($file in $files)
+{
+  $moduleFiles += $file.FullName  
+}
+
+
+Install-MyModule -ModuleName 'PSAzure' -Files $moduleFiles -Replace
+
+$moduleLocation = Get-UserModulePath
+$myModuleLocation = "$moduleLocation\PSAzure"
+
+Get-ChildItem $myModuleLocation
+
+Pop-Location
