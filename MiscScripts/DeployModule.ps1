5545b9d3c8c004c7cceafba50ac813c7c3337eee
Justin Johnson <Justin.Johnson@advent.com>
2020-05-22 11:28:58 -0400

    Fixed documentation typos

--- MiscScripts/DeployModule.ps1
+++ MiscScripts/DeployModule.ps1
@@ -1,23 +1,4 @@
-﻿<#-----------------------------------------------------------------------------
-  Beginning PowerShell Scripting for Developers
-  DeployModule.ps1
-
-  Author: Robert C. Cain | @ArcaneCode | arcanecode@gmail.com
-          http://arcanecode.com
- 
-  This module is Copyright (c) 2015 Robert C. Cain. All rights reserved.
-  The code herein is for demonstration purposes. No warranty or guarentee
-  is implied or expressly granted. 
-  This module may not be reproduced in whole or in part without the express
-  written consent of the author. 
-
-  Notes
-  This script can be used to deploy a module to the users WindowsPowershell
-  Modules folder. 
------------------------------------------------------------------------------#>
-
-
-#-----------------------------------------------------------------------------#
+﻿#-----------------------------------------------------------------------------#
 # This checks to see if the WindowsPowerShell folder is in the users
 # document folder, then checks to see if the Modules folder exists under
 # the WindowsPowerShell folder. If they do not exist, they are created. 
